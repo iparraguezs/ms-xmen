63396d9d13bb08ed8d9f9e0d76f396b59e77c2ff
Ivan Manuel Parraguez Soto <ips.nuevo@gmail.com>
2021-11-08 06:12:16 -0300

    Add version controller

---
+++ src/test/java/com/meli/xmen/integration/VersionControllerTest.java
@@ -0,0 +1,26 @@
+package com.meli.xmen.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+public class VersionControllerTest {
+    @Autowired
+    private MockMvc mvc;
+    @Test
+    public void testStats() throws Exception {
+        mvc.perform( MockMvcRequestBuilders
+                        .get("/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+}
